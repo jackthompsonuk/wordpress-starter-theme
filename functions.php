cd1d1887b4cd84619f79901ea32eec3b4998f630
Jack Thompson <github@jackthompson.me.uk>
2019-08-22 20:51:49 +0100

    setup

---
+++ functions.php
@@ -0,0 +1,196 @@
+<?php
+/**
+ * Timber starter-theme
+ * https://github.com/timber/starter-theme
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since   Timber 0.1
+ */
+
+require_once(__DIR__ . '/vendor/autoload.php');
+$timber = new \Timber\Timber();
+
+if ( ! class_exists( 'Timber' ) ) {
+	add_action( 'admin_notices', function() {
+		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
+	});
+
+	add_filter('template_include', function( $template ) {
+		return get_stylesheet_directory() . '/dist/no-timber.html';
+	});
+
+	return;
+}
+
+/**
+ * Sets the directories (inside your theme) to find .twig files
+ */
+Timber::$dirname = array( 'templates', 'views' );
+
+/**
+ * By default, Timber does NOT autoescape values. Want to enable Twig's autoescape?
+ * No prob! Just set this value to true
+ */
+Timber::$autoescape = false;
+
+
+/**
+ * We're going to configure our theme inside of a subclass of Timber\Site
+ * You can move this to its own file and include here via php's include("MySite.php")
+ */
+class WordPressStarterTheme extends Timber\Site {
+	/** Add timber support. */
+	public function __construct() {
+		add_action( 'after_setup_theme', array( $this, 'theme_supports' ) );
+		add_filter( 'timber/context', array( $this, 'add_to_context' ) );
+		add_filter( 'timber/twig', array( $this, 'add_to_twig' ) );
+		add_action( 'init', array( $this, 'register_post_types' ) );
+		add_action( 'init', array( $this, 'register_taxonomies' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'loadScripts' ) );
+		add_action( 'acf/init', array( $this, 'my_acf_init') );
+		add_action( 'phpmailer_init', array( $this, 'send_smtp_email') );
+		add_filter( 'wpcf7_autop_or_not', array( $this, '__return_false') );
+		add_filter( 'wpcf7_form_elements', array( $this, 'wpcf7_remove_span') );
+		parent::__construct();
+	}
+	/** This is where you can register custom post types. */
+	public function register_post_types() {
+
+	}
+	/** This is where you can register custom taxonomies. */
+	public function register_taxonomies() {
+
+	}
+
+	/** This is where you add some context
+	 *
+	 * @param string $context context['this'] Being the Twig's {{ this }}.
+	 */
+	public function add_to_context( $context ) {
+		$context['foo'] = 'bar';
+		$context['stuff'] = 'I am a value set in your functions.php file';
+		$context['notes'] = 'These values are available everytime you call Timber::context();';
+		$context['menu'] = new Timber\Menu();
+		$context['site'] = $this;
+		return $context;
+	}
+
+	public function theme_supports() {
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
+
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
+
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support(
+			'html5', array(
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+			)
+		);
+
+		/*
+		 * Enable support for Post Formats.
+		 *
+		 * See: https://codex.wordpress.org/Post_Formats
+		 */
+		add_theme_support(
+			'post-formats', array(
+				'aside',
+				'image',
+				'video',
+				'quote',
+				'link',
+				'gallery',
+				'audio',
+			)
+		);
+
+		add_theme_support( 'menus' );
+	}
+
+	/** This Would return 'foo bar!'.
+	 *
+	 * @param string $text being 'foo', then returned 'foo bar!'.
+	 */
+	public function myfoo( $text ) {
+		$text .= ' bar!';
+		return $text;
+	}
+
+	/** This is where you can add your own functions to twig.
+	 *
+	 * @param string $twig get extension.
+	 */
+	public function add_to_twig( $twig ) {
+		$twig->addExtension( new Twig_Extension_StringLoader() );
+		$twig->addFilter( new Twig_SimpleFilter( 'myfoo', array( $this, 'myfoo' ) ) );
+		return $twig;
+	}
+
+	public function loadScripts() {
+        wp_enqueue_script( 'app.js', get_template_directory_uri() . '/dist/app.js', array(), '1.0.0', true );
+	} 
+	
+	public function my_acf_init() {
+		if( function_exists('acf_register_block') ) {
+			
+			acf_register_block(array(
+				'name'				=> 'example',
+				'title'				=> __('Example'),
+				'description'		=> __('Example Block.'),
+				'render_callback'	=> array( $this, 'my_acf_block_render_callback'),
+				'keywords'			=> array('example'),
+			));
+
+		}
+	}
+
+	function my_acf_block_render_callback( $block, $content = '', $is_preview = false ) {
+		$slug = str_replace('acf/', '', $block['name']);
+		$context = Timber::context();
+		$context['block'] = $block;
+		$context['fields'] = get_fields();
+		$context['is_preview'] = $is_preview;
+		Timber::render( "blocks/{$slug}.twig", $context );
+	}
+
+	function send_smtp_email( $phpmailer ) {
+		$phpmailer->isSMTP();
+		$phpmailer->Host       = SMTP_HOST;
+		$phpmailer->SMTPAuth   = SMTP_AUTH;
+		$phpmailer->Port       = SMTP_PORT;
+		$phpmailer->Username   = SMTP_USER;
+		$phpmailer->Password   = SMTP_PASS;
+		$phpmailer->SMTPSecure = SMTP_SECURE;
+		$phpmailer->From       = SMTP_FROM;
+		$phpmailer->FromName   = SMTP_NAME;
+	}
+
+	function wpcf7_remove_span($content) {
+		$content = preg_replace('/<(span).*?class="\s*(?:.*\s)?wpcf7-form-control-wrap(?:\s[^"]+)?\s*"[^\>]*>(.*)<\/\1>/i', '\2', $content);
+		return $content;
+	}
+
+}
+
+new WordPressStarterTheme();
---
+++ webpack.mix.js
@@ -0,0 +1,5 @@
+const mix = require('laravel-mix');
+
+mix.js('src/js/app.js', 'dist')
+   .sass('src/scss/app.scss', 'dist')
+   .setPublicPath('dist');
